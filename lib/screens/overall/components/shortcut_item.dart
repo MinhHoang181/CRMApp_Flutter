35457a0024aa3a19ee4d95e5a40a96674922afc5
DESKTOP-1OST43C\letha <lethanhtu0704@gmail.com>
2021-06-16 16:47:32 +0700

    update  trang chủ

---
+++ lib/screens/overall/components/shortcut_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:cntt2_crm/constants/layouts.dart' as Layouts;
+import 'package:cntt2_crm/constants/fonts.dart' as Fonts;
+
+class ShortCut  extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
