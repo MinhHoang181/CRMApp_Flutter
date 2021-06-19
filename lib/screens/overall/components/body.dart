95539ad6b755a60c3321521c9ba8a8e45406e88d
Minh Hoang <phamminhhoang181@gmail.com>
2021-06-19 21:11:30 +0700

    sửa background lỗi mục trang chủ

--- lib/screens/overall/components/body.dart
+++ lib/screens/overall/components/body.dart
@@ -9,20 +9,31 @@ import 'order_pending.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SafeArea(
-        child: Column(
-          children: [
-            SalesInfo(),
-            ShortCut(),
-            PlatformInfo(),
-            OrdersPending(),
-            SizedBox(
-              height: 17,
-            )
-          ],
+    return Stack(
+      children: [
+        // FittedBox(
+        //   child: Image(
+        //     image: AssetImage('assets/images/appbar-shape.png'),
+        //   ),
+        //   clipBehavior: Clip.hardEdge,
+        //   fit: BoxFit.fill,
+        // ),
+        SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                SalesInfo(),
+                ShortCut(),
+                PlatformInfo(),
+                OrdersPending(),
+                SizedBox(
+                  height: 17,
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
